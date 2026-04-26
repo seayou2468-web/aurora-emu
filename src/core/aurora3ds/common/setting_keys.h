c0bd83af7e3a5ab341378dccfddfd15a8cee00d5
seayou2468-web <seayou2468@gmail.com>
2026-04-26 20:24:44 +0900

    Import missing Aurora3DS headers from Azahar

---
+++ src/core/aurora3ds/common/setting_keys.h
@@ -0,0 +1,19 @@
+// Copyright Citra Emulator Project / Azahar Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#pragma once
+
+#include <array>
+#include <boost/hana/string.hpp>
+
+namespace Settings {
+
+namespace Keys {
+static const std::array<const char*, 0> keys_array{};
+}
+
+namespace HKeys {
+}
+
+} // namespace Settings
