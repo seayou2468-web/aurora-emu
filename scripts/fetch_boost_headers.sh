62648de78b48c6af445973dd78e4e79098a7fd3b
seayou2468-web <seayou2468@gmail.com>
2026-04-28 16:57:21 +0900

    ios: provision zstd headers and add explicit microprofile include

--- scripts/fetch_boost_headers.sh
+++ scripts/fetch_boost_headers.sh
@@ -7,20 +7,38 @@ BOOST_VERSION="1.87.0"
 BOOST_UNDERSCORE="${BOOST_VERSION//./_}"
 BOOST_TAR="boost_${BOOST_UNDERSCORE}.tar.bz2"
 BOOST_URL="https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_TAR}"
+ZSTD_VERSION="1.5.7"
+ZSTD_TAR="zstd-${ZSTD_VERSION}.tar.gz"
+ZSTD_URL="https://github.com/facebook/zstd/releases/download/v${ZSTD_VERSION}/${ZSTD_TAR}"
+ZSTD_INCLUDE_DIR="${ROOT_DIR}/Dependicies/Sources/zstd/include"
 
-if [[ -f "${BOOST_INCLUDE_DIR}/serialization/access.hpp" ]]; then
-  echo "Boost headers already present: ${BOOST_INCLUDE_DIR}"
-  exit 0
-fi
-
-echo "Fetching Boost headers ${BOOST_VERSION}..."
 TMP_DIR="$(mktemp -d)"
 trap 'rm -rf "${TMP_DIR}"' EXIT
 
-curl -L "${BOOST_URL}" -o "${TMP_DIR}/${BOOST_TAR}"
-tar -xjf "${TMP_DIR}/${BOOST_TAR}" -C "${TMP_DIR}"
+if [[ -f "${BOOST_INCLUDE_DIR}/serialization/access.hpp" ]]; then
+  echo "Boost headers already present: ${BOOST_INCLUDE_DIR}"
+else
+  echo "Fetching Boost headers ${BOOST_VERSION}..."
+  curl -L "${BOOST_URL}" -o "${TMP_DIR}/${BOOST_TAR}"
+  tar -xjf "${TMP_DIR}/${BOOST_TAR}" -C "${TMP_DIR}"
 
-mkdir -p "${ROOT_DIR}/Dependicies/Sources/boost/include"
-cp -R "${TMP_DIR}/boost_${BOOST_UNDERSCORE}/boost" "${ROOT_DIR}/Dependicies/Sources/boost/include/"
+  mkdir -p "${ROOT_DIR}/Dependicies/Sources/boost/include"
+  cp -R "${TMP_DIR}/boost_${BOOST_UNDERSCORE}/boost" "${ROOT_DIR}/Dependicies/Sources/boost/include/"
 
-echo "Installed Boost headers to ${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
+  echo "Installed Boost headers to ${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
+fi
+
+if [[ -f "${ZSTD_INCLUDE_DIR}/zstd.h" ]]; then
+  echo "Zstd headers already present: ${ZSTD_INCLUDE_DIR}"
+else
+  echo "Fetching Zstd headers ${ZSTD_VERSION}..."
+  curl -L "${ZSTD_URL}" -o "${TMP_DIR}/${ZSTD_TAR}"
+  tar -xzf "${TMP_DIR}/${ZSTD_TAR}" -C "${TMP_DIR}"
+
+  mkdir -p "${ZSTD_INCLUDE_DIR}"
+  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zstd.h" "${ZSTD_INCLUDE_DIR}/"
+  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zdict.h" "${ZSTD_INCLUDE_DIR}/"
+  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zstd_errors.h" "${ZSTD_INCLUDE_DIR}/"
+
+  echo "Installed Zstd headers to ${ZSTD_INCLUDE_DIR}"
+fi
