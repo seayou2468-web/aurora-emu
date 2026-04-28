b3401d3a954e50d6b381e87d035dc76334c45e51
seayou2468-web <seayou2468@gmail.com>
2026-04-28 20:07:23 +0900

    ios(ci): fetch glslang headers for SPIRV includes

--- scripts/fetch_third_party_headers.sh
+++ scripts/fetch_third_party_headers.sh
@@ -15,6 +15,11 @@ VMA_TAR="VulkanMemoryAllocator-${VMA_VERSION}.tar.gz"
 VMA_URL="https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/archive/refs/tags/v${VMA_VERSION}.tar.gz"
 VMA_INCLUDE_DIR="${ROOT_DIR}/Dependicies/Sources/vma/include/vma"
 VMA_HEADER="${VMA_INCLUDE_DIR}/vk_mem_alloc.h"
+GLSLANG_VERSION="vulkan-sdk-1.3.296.0"
+GLSLANG_TAR="glslang-${GLSLANG_VERSION}.tar.gz"
+GLSLANG_URL="https://github.com/KhronosGroup/glslang/archive/refs/tags/${GLSLANG_VERSION}.tar.gz"
+GLSLANG_INCLUDE_ROOT="${ROOT_DIR}/Dependicies/Sources/glslang/include"
+GLSLANG_SPIRV_HEADER="${GLSLANG_INCLUDE_ROOT}/SPIRV/GlslangToSpv.h"
 
 if [[ -f "${BOOST_INCLUDE_DIR}/serialization/access.hpp" && -f "${BOOST_INCLUDE_DIR}/align/align.hpp" ]]; then
   echo "Boost headers already present: ${BOOST_INCLUDE_DIR}"
@@ -39,4 +44,18 @@ else
   mkdir -p "${VMA_INCLUDE_DIR}"
   cp "${TMP_DIR}/VulkanMemoryAllocator-${VMA_VERSION}/include/vk_mem_alloc.h" "${VMA_HEADER}"
   echo "Installed VMA header to ${VMA_HEADER}"
+fi
+
+if [[ -f "${GLSLANG_SPIRV_HEADER}" ]]; then
+  echo "glslang headers already present: ${GLSLANG_SPIRV_HEADER}"
+else
+  echo "Fetching glslang headers ${GLSLANG_VERSION}..."
+  curl -L "${GLSLANG_URL}" -o "${TMP_DIR}/${GLSLANG_TAR}"
+  tar -xzf "${TMP_DIR}/${GLSLANG_TAR}" -C "${TMP_DIR}"
+
+  mkdir -p "${GLSLANG_INCLUDE_ROOT}"
+  rm -rf "${GLSLANG_INCLUDE_ROOT}/glslang" "${GLSLANG_INCLUDE_ROOT}/SPIRV"
+  cp -R "${TMP_DIR}/glslang-${GLSLANG_VERSION}/glslang" "${GLSLANG_INCLUDE_ROOT}/"
+  cp -R "${TMP_DIR}/glslang-${GLSLANG_VERSION}/SPIRV" "${GLSLANG_INCLUDE_ROOT}/"
+  echo "Installed glslang headers to ${GLSLANG_INCLUDE_ROOT}"
 fi
