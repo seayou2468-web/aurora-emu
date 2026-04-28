8df946467bf5f153444e2c86967ed1af25ce23ea
seayou2468-web <seayou2468@gmail.com>
2026-04-28 19:18:25 +0900

    ios(ci): fetch VMA header and add include path

--- scripts/fetch_third_party_headers.sh
+++ scripts/fetch_third_party_headers.sh
@@ -10,6 +10,11 @@ BOOST_UNDERSCORE="${BOOST_VERSION//./_}"
 BOOST_TAR="boost_${BOOST_UNDERSCORE}.tar.bz2"
 BOOST_URL="https://archives.boost.io/release/${BOOST_VERSION}/source/${BOOST_TAR}"
 BOOST_INCLUDE_DIR="${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
+VMA_VERSION="3.3.0"
+VMA_TAR="VulkanMemoryAllocator-${VMA_VERSION}.tar.gz"
+VMA_URL="https://github.com/GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator/archive/refs/tags/v${VMA_VERSION}.tar.gz"
+VMA_INCLUDE_DIR="${ROOT_DIR}/Dependicies/Sources/vma/include/vma"
+VMA_HEADER="${VMA_INCLUDE_DIR}/vk_mem_alloc.h"
 
 if [[ -f "${BOOST_INCLUDE_DIR}/serialization/access.hpp" && -f "${BOOST_INCLUDE_DIR}/align/align.hpp" ]]; then
   echo "Boost headers already present: ${BOOST_INCLUDE_DIR}"
@@ -22,4 +27,16 @@ else
   rm -rf "${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
   cp -R "${TMP_DIR}/boost_${BOOST_UNDERSCORE}/boost" "${ROOT_DIR}/Dependicies/Sources/boost/include/"
   echo "Installed Boost headers to ${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
+fi
+
+if [[ -f "${VMA_HEADER}" ]]; then
+  echo "VMA header already present: ${VMA_HEADER}"
+else
+  echo "Fetching VMA headers ${VMA_VERSION}..."
+  curl -L "${VMA_URL}" -o "${TMP_DIR}/${VMA_TAR}"
+  tar -xzf "${TMP_DIR}/${VMA_TAR}" -C "${TMP_DIR}"
+
+  mkdir -p "${VMA_INCLUDE_DIR}"
+  cp "${TMP_DIR}/VulkanMemoryAllocator-${VMA_VERSION}/include/vk_mem_alloc.h" "${VMA_HEADER}"
+  echo "Installed VMA header to ${VMA_HEADER}"
 fi
