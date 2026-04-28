8ca93beac54848f2cf79fc0a7e44d274b2c71bc1
seayou2468-web <seayou2468@gmail.com>
2026-04-28 19:20:26 +0900

    ios: add OpenSSL SPM dependency pin and linkage

--- SharedDependencies/Package.swift
+++ SharedDependencies/Package.swift
@@ -11,14 +11,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Azoy/zlib-spm", branch: "master"),
-        .package(url: "https://github.com/jarrodnorwell/zstd", branch: "dev")
+        .package(url: "https://github.com/jarrodnorwell/zstd", branch: "dev"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL", branch: "main")
     ],
     targets: [
         .target(
             name: "SharedDependencies",
             dependencies: [
                 .product(name: "libzstd", package: "zstd"),
-                .product(name: "libseekable_format", package: "zstd")
+                .product(name: "libseekable_format", package: "zstd"),
+                .product(name: "OpenSSL", package: "OpenSSL")
             ]
         )
     ]
