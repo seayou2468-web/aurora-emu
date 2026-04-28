6a2d03275ff6778a75bfb24362e0bc4f234c9bb7
seayou2468-web <seayou2468@gmail.com>
2026-04-28 18:16:27 +0900

    ios: migrate zstd to SwiftPM pins and add Package.resolved

--- scripts/fetch_third_party_headers.sh
+++ scripts/fetch_third_party_headers.sh
@@ -23,23 +23,3 @@ else
   cp -R "${TMP_DIR}/boost_${BOOST_UNDERSCORE}/boost" "${ROOT_DIR}/Dependicies/Sources/boost/include/"
   echo "Installed Boost headers to ${ROOT_DIR}/Dependicies/Sources/boost/include/boost"
 fi
-
-ZSTD_VERSION="1.5.7"
-ZSTD_TAR="zstd-${ZSTD_VERSION}.tar.gz"
-ZSTD_URL="https://github.com/facebook/zstd/releases/download/v${ZSTD_VERSION}/${ZSTD_TAR}"
-ZSTD_INCLUDE_ROOT="${ROOT_DIR}/Dependicies/Sources/zstd/include"
-
-if [[ -f "${ZSTD_INCLUDE_ROOT}/zstd.h" && -f "${ZSTD_INCLUDE_ROOT}/seekable_format/zstd_seekable.h" ]]; then
-  echo "Zstd headers already present: ${ZSTD_INCLUDE_ROOT}"
-else
-  echo "Fetching Zstd headers ${ZSTD_VERSION}..."
-  curl -L "${ZSTD_URL}" -o "${TMP_DIR}/${ZSTD_TAR}"
-  tar -xzf "${TMP_DIR}/${ZSTD_TAR}" -C "${TMP_DIR}"
-
-  mkdir -p "${ZSTD_INCLUDE_ROOT}/seekable_format"
-  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zstd.h" "${ZSTD_INCLUDE_ROOT}/"
-  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zdict.h" "${ZSTD_INCLUDE_ROOT}/"
-  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/lib/zstd_errors.h" "${ZSTD_INCLUDE_ROOT}/"
-  cp "${TMP_DIR}/zstd-${ZSTD_VERSION}/contrib/seekable_format/zstd_seekable.h" "${ZSTD_INCLUDE_ROOT}/seekable_format/"
-  echo "Installed Zstd headers to ${ZSTD_INCLUDE_ROOT}"
-fi
