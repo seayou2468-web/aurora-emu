b6c02560d6f03efa4faf0038ca757329299c239d
seayou2468-web <seayou2468@gmail.com>
2026-04-30 10:35:24 +0900

    Fix bridging header missing include and exclude non-source core assets

--- Platforms/AuroraIOS/swift/Bridge/AuroraSwift-Bridging-Header.h
+++ Platforms/AuroraIOS/swift/Bridge/AuroraSwift-Bridging-Header.h
@@ -1,3 +1,2 @@
 #import "../../objc/Bridge/AURCoreSessionFactory.h"
 #import "../../objc/Controllers/AUR3DSEmulatorViewController.h"
-#import "../../../../src/core/bridge_cores/aurora3ds/CytrusEmulator.h"
