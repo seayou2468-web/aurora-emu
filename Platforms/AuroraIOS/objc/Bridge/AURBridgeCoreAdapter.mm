eeef394642ec115d357f5ff3c351e9ec88937e24
seayou2468-web <seayou2468@gmail.com>
2026-04-30 11:17:42 +0900

    Fix AURBridgeCoreAdapter ivars and protocol property synthesis

--- Platforms/AuroraIOS/objc/Bridge/AURBridgeCoreAdapter.mm
+++ Platforms/AuroraIOS/objc/Bridge/AURBridgeCoreAdapter.mm
@@ -2,12 +2,15 @@
 
 @interface AURBridgeCoreAdapter () {
     EmulatorCoreHandle *_coreHandle;
+    BOOL _running;
     EmulatorVideoSpec _videoSpec;
 }
 @end
 
 @implementation AURBridgeCoreAdapter
 @synthesize coreType = _coreType;
+@synthesize running = _running;
+@synthesize videoSpec = _videoSpec;
 
 - (instancetype)initWithCoreType:(EmulatorCoreType)coreType {
     self = [super init];
