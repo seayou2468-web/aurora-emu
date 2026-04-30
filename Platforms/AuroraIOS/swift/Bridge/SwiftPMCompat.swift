96a19eaf18dae2ea354b2e2b65e46b6a0daf4caa
seayou2468-web <seayou2468@gmail.com>
2026-04-30 10:11:27 +0900

    SwiftPM: add compatibility shim for ObjC bridge symbols in package build

---
+++ Platforms/AuroraIOS/swift/Bridge/SwiftPMCompat.swift
@@ -0,0 +1,38 @@
+#if SWIFT_PACKAGE
+import UIKit
+import Foundation
+
+typealias EmulatorCoreType = UInt32
+let EMULATOR_CORE_TYPE_3DS: EmulatorCoreType = 0
+let EMULATOR_CORE_TYPE_NDS: EmulatorCoreType = 1
+let EMULATOR_CORE_TYPE_GBA: EmulatorCoreType = 2
+let EMULATOR_CORE_TYPE_GB: EmulatorCoreType = 3
+let EMULATOR_CORE_TYPE_NES: EmulatorCoreType = 4
+
+typealias EmulatorKey = UInt32
+
+@objcMembers
+final class AURCoreSession: NSObject {
+    func loadROM(at: URL) -> Bool { true }
+    func loadBIOS(atPath: String) -> Bool { true }
+    func setKey(_ key: EmulatorKey, pressed: Bool) {}
+    func stepFrame() {}
+    func lastError() -> String? { nil }
+}
+
+@objcMembers
+final class AURCoreSessionFactory: NSObject {
+    static func session(with coreType: EmulatorCoreType) -> AURCoreSession { AURCoreSession() }
+}
+
+final class AUREmulatorViewController: UIViewController {
+    init(romURL: URL, coreType: EmulatorCoreType) {
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = .black
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+#endif
